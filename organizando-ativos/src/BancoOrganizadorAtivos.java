9ba013e44073552e399d89ed8386e1510728b8c3
Bruno Rodrigues Peixoto <134175465+brunopeixotoo@users.noreply.github.com>
2023-09-14 21:12:54 -0400

    Update BancoOrganizadorAtivos.java
    Update do desafio.

--- organizando-ativos/src/BancoOrganizadorAtivos.java
+++ organizando-ativos/src/BancoOrganizadorAtivos.java
@@ -11,13 +11,12 @@ public class BancoOrganizadorAtivos {
         //Entrada da quantidade de ativos.
         System.out.println("Qual a quantidade de ativos? ");
         int quantidadeAtivos = entradaDados.nextInt();
-        quantidadeAtivos+=1;
+        scanner.nextLine();
 
         //Entrada do nome dos ativos e colocando na Array.
         System.out.println("Digite o nome dos ativos: ");
         for(int i = 0; i < quantidadeAtivos; i++) {
             String codigoAtivo = entradaDados.nextLine();
-            String listOrdem = codigoAtivo.toUpperCase();
             ativos.add(listOrdem);
         }
         Collections.sort(ativos);
